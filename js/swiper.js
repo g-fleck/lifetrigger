d9a8ece18bf4b344b0e96f3785500c427c4e1000
g-fleck <74872831+g-fleck@users.noreply.github.com>
2020-11-22 17:42:27 +0000

    Add files via upload

---
+++ js/swiper.js
@@ -0,0 +1,61 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    centeredSlides: true,
+    breakpoints: {
+        800: {
+            slidesPerView: 2,
+            centeredSlides: false
+        },
+        1100: {
+            slidesPerView: 3,
+            centeredSlides: true
+        }
+
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+    autoplay: {
+        delay: 10000,
+        disableOnInteraction: false,
+    }
+});
+
+var swiperMarcas = new Swiper('.swiper-container-marcas', {
+    // Optional parameters
+    loop: true,
+    // centeredSlides: true,
+    speed: 15000,
+    spaceBetween: 50,
+    slidesPerView: 'auto',
+    loopedSlides: 11,
+    slidesPerGroup: 11,
+    freeMode: true,
+    autoplay: {
+        delay: 1,
+        disableOnInteraction: false,
+    }
+})
+
+  var swiperMarcas2 = new Swiper('.swiper-container-marcas2', {
+    // Optional parameters
+    loop: true,
+    // centeredSlides: true,
+    speed: 15000,
+    spaceBetween: 50,
+    slidesPerView: 'auto',
+    loopedSlides: 11,
+    slidesPerGroup: 11,
+    freeMode: true,
+    autoplay: {
+        delay: 1,
+        disableOnInteraction: false,
+        reverseDirection: true
+    }
+  })
